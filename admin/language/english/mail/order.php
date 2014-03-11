a4b90e3194ee38d355fe7f6124e2759eb55f0bc3
Tim Faulkner <tim@ants4t.co.uk>
2014-03-11 15:26:23 +0000

    Redeem email copy changes

--- admin/language/english/mail/order.php
+++ admin/language/english/mail/order.php
@@ -6,7 +6,9 @@ $_['text_date_added']   = 'Date Ordered:';
 $_['text_order_status'] = 'Your order has been updated to the following status:';
 $_['text_comment']      = 'The comments for your order are:';
 $_['text_link']         = 'To view your order click on the link below:';
-$_['text_redeem_download'] = 'To view your redeemable voucher click on the link(s) below:';
+$_['text_redeem_download'] = 'YOUR VOUCHER(S)
+
+To view your voucher and for instructions on how to redeem, please view the attached vouchers or click on the link(s) below:';
 $_['text_redeem']       = 'Your instore redeemable codes are:';
 $_['text_footer']       = 'Please reply to this email if you have any questions.';
 ?>
